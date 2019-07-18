3b228b81c3869121105d019022484369bbb4b51d
岩原こうき <kouki.iwahara19860712@gmail.com>
2019-07-18 10:37:26 +0900

    first

---
+++ plugins/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: "AIzaSyChIAauzlWc9EV6kidyW55v3Z7u1XCS1gw",
+    authDomain: "nuxt-firebase-57e4b.firebaseapp.com",
+    databaseURL: "https://nuxt-firebase-57e4b.firebaseio.com",
+    projectId: "nuxt-firebase-57e4b",
+    storageBucket: "nuxt-firebase-57e4b.appspot.com",
+    messagingSenderId: "607808883297",
+    appId: "1:607808883297:web:9aa350f2e699a151"
+  })
+}
+
+export default firebase

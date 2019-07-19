9e590677313c72a935077672166ea8f653c4a856
岩原こうき <kouki.iwahara19860712@gmail.com>
2019-07-19 23:04:45 +0900

    ホーム画面、マイページのイメージ実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,46 @@
 <template>
   <div class="container">
+    <Head/>
     <div class="main">
       <h1>思い出の本投稿アプリホーム画面</h1>
+      <p>textが入りますtextが入りますtextが入ります</p>
+      <img class="main-img" src="../book.jpg" alt="">
+      <nav class="main-link">
+        <nuxt-link to="/signup">サインアップ画面</nuxt-link> /
+        <nuxt-link to="/signin">サインイン画面</nuxt-link>
+      </nav>
     </div>
-    <nuxt-link to="/signup">サインアップ画面</nuxt-link> /
-    <nuxt-link to="/signin">サインイン画面</nuxt-link>
+    <div class="content">
+      <display-posts/>
+    </div>
   </div>
 </template>
 
 <script>
+import Head from '~/components/Head.vue'
+import DisplayPosts from '~/components/DisplayPosts.vue'
+
 export default {
-  
+  components: {
+    Head,
+    DisplayPosts
+  }
 }
 </script>
 
 <style scoped>
 .container {
   text-align: center;
+  height: 480px;
+}
+
+.main-img {
+  height: 300px;
+  width: 100%;
+}
+
+.main-link {
+  font-size: 20px;
+  margin-bottom: 30px;
 }
 </style>

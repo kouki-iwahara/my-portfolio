de7cc7973ba1ac03ab411ed54e56ed62a2d6bc5b
岩原こうき <kouki.iwahara19860712@gmail.com>
2019-07-19 23:02:53 +0900

    メールとgoogleでのサインイン機能の実装

--- pages/signin.vue
+++ pages/signin.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    未実装
+  <div id="signin">
+    <h1>SignIn!!</h1>
+    <input type="text" placeholder="example@gmail.com" v-model="userMail">
+    <input type="password" placeholder="パスワード" v-model="password">
+    <button @click="signIn">サインイン</button>
+
+    <button @click="googleLogin">Googleアカウントでログイン</button>
+    <nuxt-link to="/">ホーム画面</nuxt-link>
+    <nuxt-link to="/signup">サインアップ画面</nuxt-link>
   </div>
 </template>
 
@@ -8,6 +15,51 @@
 import firebase from '@/plugins/firebase'
 
 export default {
+  name: 'Signin',
+  asyncData() {
+    return {
+      userMail: '',
+      password: '',
+    }
+  },
+  methods: {
+    signIn() {
+      firebase.auth()
+      .signInWithEmailAndPassword(this.userMail, this.password)
+      .then(() => {
+        this.$router.push({path: '/mypage'});
+      })
+      .catch(error => {
+        alert(error.message)
+      })
+    },
+    googleLogin() {
+      firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then(() => {
+        const user = firebase.auth().currentUser;
+        this.$store.commit('setDisplayName', user.displayName);
+        this.$router.push({path: '/mypage'});
+      })
+      .catch((error) => {
+        alert(error)
+      })
+    }
+  }
   
 }
 </script>
+
+
+<style scoped>
+#signin {
+  margin-top: 20px;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+}
+input {
+  margin: 10px 0;
+  padding: 10px;
+}
+</style>

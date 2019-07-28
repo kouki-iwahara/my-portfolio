30741ac95200f9e39721efd36957747f2dc07035
岩原こうき <kouki.iwahara19860712@gmail.com>
2019-07-28 22:22:36 +0900

    フォームの作成

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,15 +1,52 @@
 <template>
   <div id="signup">
-    <h1>SignUp!!</h1>
-    <span>メールとパスワードでログイン登録</span>
-    <input type="text" placeholder="表示名" v-model="userName">
-    <input type="text" placeholder="example@gmail.com" v-model="userMail">
-    <input type="password" placeholder="パスワード" v-model="password">
-    <button @click="signUp">登録</button>
+    <h1>ユーザー登録</h1>
 
+    <div class="register-form">
+      <fieldset>
+        <legend>メールとパスワードで登録</legend>
+        <form @submit.prevent="signUp">
+          <div class="user-name">
+            <label for="user-name">
+              <p>名前(ニックネーム)</p>
+              <input type="text" id="user-name" placeholder="表示名" v-model="userName" required>
+            </label>
+          </div>
+          <!-- /user-name -->
+          <div class="user-mail">
+            <label for="user-mail">
+              <p>Eメール</p>
+              <input type="email" id="user-mail" placeholder="example@gmail.com" v-model="userMail" required>
+            </label>
+          </div>
+          <!-- /user-name -->
+          <div class="password">
+            <label for="password">
+              <p>パスワード</p>
+              <input type="password" id="password" placeholder="パスワード" v-model="password" required>
+            </label>
+          </div>
+          <!-- /password -->
+          <div class="user-image">
+            <p>アイコン画像</p>
+            <input type="file">
+            <input type="button" value="画像をアップロード">
+
+            <div class="user-image_preview">
+              <img class="userImage" alt="" width="200" height="200">
+            </div>
+            <!-- /user-image_preview -->
+          </div>
+          <!-- /user-image -->
+          <input type="submit" value="登録">
+        </form>
+      </fieldset>
+    </div>
+    <!-- /register-form -->
     <nuxt-link to="/">ホーム画面</nuxt-link>
     <nuxt-link to="/signin">サインイン画面</nuxt-link>
   </div>
+  <!-- /signup -->
 </template>
 
 <script>
